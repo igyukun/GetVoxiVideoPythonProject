d68738d39f6c68455fcf83c469aacf13f51baa78
igyukun <igorkun@gmail.com>
2022-12-27 11:06:15 +0200

    Working version with support of:  1. Python 3.5+ and OpenCV 4.0+  2. Histogram drawing with matplotlib  3. Still image capturing

--- main.py
+++ main.py
@@ -1,76 +1,77 @@
-# Read the video stream from VOxI camera
-# through the USB3 interface
-# and display the video with the frame rate
-# min/max and mean value in the 14-bit range
+    # Read and display the video stream from VOxI camera using the USB3 interface in Y
 
+import calendar
 import cv2
 import numpy as np
 import time
-import utils as u
+from utils import *
 
-# used to record the time when we processed last frame
+
+# The time of the last and the current frames
 prev_frame_time = 0
-
-# used to record the time at which we processed current frame
-new_frame_time = 0
+new_frame_time  = 0
 
 # video statistics globals:
-sFps = 0   # frame rate string
+sFps    = 0   # frame rate string
 meanval = 0 # frame mean value
-minval = 0  # frame minimum value
-maxval = 0  # frame maximum value
-
+minval  = 0  # frame minimum value
+maxval  = 0  # frame maximum value
+curfps  = 0
 # seconds counter to be used for refreshing the stats data on the screen
 tic = time.time()
 toc = time.time()
 # defines the moving averaging window (number of frames)
-avg_window = 1024
+avg_window = 400
 # the list of the individual frames durations with the length of up to 'avg_window'
 frm_time_list = [0]
+# OSD toggler
+toggleOSD = True
 
 # Create a video capturing device instance
-cam0 = cv2.VideoCapture(0)
+cam0 = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
 # Convert the captured image to RGB
 cam0.set(cv2.CAP_PROP_CONVERT_RGB, 0)
 
-# Video presentation loop
-# Iterate as long as a video capturing device is opened,
-# the frame can be read or until 'Q' key is pressed
+s, img0 = cam0.read()
+
+# Video presentation loop:
+# Terminates when video capturing device is closed, the frame cannot be read or 'Q/q' key is pressed
 while cam0.isOpened():
 
     # get the current time
     toc = time.time()
-
     # Read a new video frame
     s, img0 = cam0.read()
-
     # break if could not read the frame (e.g. end of video or camera goes offline
     if not s:
         break
-
     # time when we finished the current frame acquisition
     new_frame_time = time.time()
 
-    # Calculate the fps and cast the result to int
+    # Calculate the fps and round the result to the closest integer
     # the condition is set to prevent the division by '0' error
     if not new_frame_time == prev_frame_time:
-        currfps = 1 / (new_frame_time - prev_frame_time)
+        currfps = round(1 / (new_frame_time - prev_frame_time))
 
     # Call utils.moving_avg_window function to filter down the frame rate fluctuations
-    frm_time_list, meanfps = u.moving_avg_window(frm_time_list, avg_window, currfps)
-
+    # frm_time_list, meanfps = moving_avg_window(frm_time_list, avg_window, currfps)
+    frm_time_list, meanfps = moving_avg_window(frm_time_list, avg_window, currfps)
     # assign the current time to the previous frame time
     prev_frame_time = new_frame_time
 
     # Scale down the video data from 16-bit to 14-bit format
-    I = np.asarray(img0, dtype='>B').view(np.uint16) - 49152
+    I = np.asarray(img0).view(np.uint16) - 49152
+    # No need to convert the data type with the use of DSHOW video capturing
+    # I = np.asarray(img0, dtype='>B').view(np.uint16) - 49152
+
     # Arrange the video frame data as two-dimensional array with 640 elements in a row
     img1 = np.reshape(I, (-1, 640))
 
     # Apply a linear DRC to scale the video down from 14-bit to 8-bit
-    gray = u.linear_DRC(img1)
+    gray = linear_DRC(img1)
 
-    # Update the video stats once in every second
+    # Update the video stats once a second
     if (toc - tic) >= 1.0:
         sFps = int(meanfps)
         minval = np.min(img1)
@@ -80,14 +81,32 @@ while cam0.isOpened():
         tic = time.time()
 
     # Draw the video statistics on top of the video frame (frame rate, minimum, maximum and mean values)
-    u.draw_stats(gray, sFps, minval, maxval, meanval)
+    if toggleOSD:
+        draw_stats(gray, sFps, minval, maxval, meanval)
 
     # Display compressed video data with the OSD text
+    cv2.namedWindow("VOXI USB video", cv2.WINDOW_NORMAL)
     cv2.imshow('VOXI USB video', gray.astype('uint8'))
 
-    # Exit the video displaying loop condition
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Process the keypress events
+    key = cv2.pollKey()
+    # terminate application
+    if key == ord('q') or key == ord('Q') or cv2.getWindowProperty('VOXI USB video',cv2.WND_PROP_VISIBLE) == 0:
         break
+    # capture the current frame into a file
+    if key == ord('c') or key == ord('C'):
+        cv2.imwrite(f'Capture_{calendar.timegm(time.gmtime())}.png', gray)
+    # pause the video and plot the current frame histogram
+    if key == ord('p') or key == ord('P'):
+        draw_histogram(img1, minval, maxval)
+        # pause until the next keypress
+        cv2.waitKey(0)
+    # press 'o' key to toggle the OSD on and off
+    if key == ord('o') or key == ord('O'):
+        if toggleOSD:
+            toggleOSD = False
+        else:
+            toggleOSD = True
 
 # release and destroy video acquisition device instance
 cam0.release()
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
-import re
-
+import matplotlib.pyplot as plt
 
 def drawtext(framedata, coords, text="No data", ):
     """
@@ -9,11 +8,14 @@ def drawtext(framedata, coords, text="No data", ):
     :param framedata:   the video frame
     :param coords:      X, Y coordinates of the left-bottom corner of a text rectangle
     :param text:        the text to be printed (default = "No data")
+    :return:            none
     """
     # select font type
-    font = cv2.FONT_HERSHEY_PLAIN
-    # put text to the video device
-    cv2.putText(framedata, text, coords, font, 1.2, (0, 0, 0), 2, cv2.FILLED)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    # put thin black text on top of white thick text to get the text visible on any background
+    cv2.putText(framedata, text, coords, font, 0.5, (255, 255, 255), 3, cv2.LINE_AA)
+    cv2.putText(framedata, text, coords, font, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 
 
 def draw_stats(gray, fps, min_val, max_val, mean_val):
@@ -24,11 +26,32 @@ def draw_stats(gray, fps, min_val, max_val, mean_val):
     :param min_val:     video array minimum
     :param max_val:     video array maximum
     :param mean_val:    video array mean
+    :return:            none
     """
     drawtext(gray, (10, 400), str(f"Frame rate: {fps} fps"))
     drawtext(gray, (10, 430), str(f"min: {min_val} DL <-> max: {max_val} DL."))
     drawtext(gray, (10, 460), str(f"Frame mean: {mean_val} DL."))
-    drawtext(gray, (10, 20), "Press 'Q' key to terminate")
+    drawtext(gray, (10, 20), "'Q/q' - terminate")
+    drawtext(gray, (10, 50), "'C/c' - capture frame")
+    drawtext(gray, (10, 80), "'P/p' - pause and plot")
+    drawtext(gray, (10, 110), "'O/o' - toggle OSD")
+
+
+def draw_histogram (img, minval, maxval):
+    """
+    Draws the signal frequencies histogram calculated from the 14-bit image data
+    :param img:     data matrix
+    :param minval:  minimal value (to define the low limit of the data range)
+    :param maxval:  maximal value (to define the high limit of the the data range)
+    :return:        none
+    """
+    plt.hist(img.reshape(-1), density=True, bins=1000, range=(minval - 50, maxval + 50),
+             color='green', histtype='stepfilled')
+    plt.title("VOXI USB video")
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.grid(color='red', linestyle='--', linewidth=0.5)
+    plt.show()
 
 
 def moving_avg_window (lst, windowsize, newval):
@@ -39,7 +62,7 @@ def moving_avg_window (lst, windowsize, newval):
     :param lst:         the list of values to average
     :param windowsize:  the maximal size of the list allowed
     :param newval:      he most current value to append to the list
-    :return: an updated list and its mean value
+    :return:            an updated list and its mean value
     """
     if len(lst) == windowsize + 1:
         lst.pop(0)
@@ -51,10 +74,12 @@ def linear_DRC(img1):
     """
     Scales down the video data to 8-bit for visual representation
     using a linear dynamic range compression mechanism
-    :param img1: the video frame matrix
-    :return: the data compressed to 8-bit
+    :param img1:    the video frame matrix
+    :return:        the data compressed to 8-bit
     """
     data = np.array(img1, dtype='f')
     data = data - data.min()
     data = data / data.max()
     return 255 * data
+
+
